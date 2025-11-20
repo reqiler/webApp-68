1ace44205e1f8739e7e86e65db8a8fc09c26133e
reqiler <f.kung49@gmail.com>
2025-11-20 09:45:22 +0700

    add HTML structure and PHP greeting in index.php

---
+++ helloworld/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My-Web</title>
+</head>
+
+<body>
+    <?php
+    echo "Hello World";
+    echo "<br>";
+    echo "<h2>My name is F</h2>";
+    ?>
+</body>
+
+</html>
